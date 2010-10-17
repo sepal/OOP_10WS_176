9dc4dc892235603cd81d089eba0aa4c3dc4bbb99
mo-oz <Monika@.(none)>
2010-10-17 20:05:38 +0200

    fehler korrigiert, getStock() erledigt

--- aufgabe1/src/OOP/Lager.java
+++ aufgabe1/src/OOP/Lager.java
@@ -1,8 +1,6 @@
 package OOP;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Set;
 
 import OOP.ProduktVerwaltung.Produkt;
 
@@ -12,42 +10,41 @@ public class Lager {
 	private HashMap<Produkt, Integer> stock;
 	private int aktuell;
 	
-	private Lager() {
+	public Lager() {
 		stock = new HashMap<Produkt, Integer>();
 	}
 	
-	/*muss besser gehen...*/
-	public Set<Produkt> getStock() {
-		return stock.keySet();
+	public HashMap<Produkt, Integer> getStock() {
+		return stock;
 	}
 	
 	public Integer getProduktInStock(Produkt p) {
 		return stock.get(p);
 	}
 	
-	public void incrementStock(Produkt p, int anzahl) {
-		aktuell = getProduktInStock(p);
-		
+	public void incrementStock(Produkt p, int anzahl) {		
 		if(stock.containsKey(p)) {
+			aktuell = getProduktInStock(p);
 			aktuell += anzahl;
 			stock.put(p, aktuell);
 		} else {
-			throw new ProductDoesNotExistException("Product "+p+ "does not exist yet!");
+			aktuell = anzahl;
+			stock.put(p, aktuell);
 		}
 	}
 	
-	public void decrementStock(Produkt p, int anzahl) {
-		aktuell = getProduktInStock(p);
-		
+	public void decrementStock(Produkt p, int anzahl) {		
 		if(stock.containsKey(p)) {
-			aktuell -= anzahl;
-			stock.put(p, aktuell);
+			aktuell = getProduktInStock(p);
+			
+			if(aktuell > 0) {
+				aktuell -= anzahl;
+				stock.put(p, aktuell);
+			} else {
+				throw new ProductDoesNotExistException("The amount of product "+p+" is 0.");
+			}
 		} else {
 			throw new ProductDoesNotExistException("Product "+p+ "does not exist yet!");
 		}
 	}
-	
-//TODO: incrementStock(Produkt, int Anzahl)
-//TODO: decrementStock(Produkt, int Anzahl)
-//TODO: getStock(Produkt), return Hashmap
 }
