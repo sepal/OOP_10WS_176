8a3e68da3e98d29cbbea23beef9a13d7166e0956
Sepal <sep.gil@gmail.com>
2010-10-17 18:29:40 +0200

    Konfiguration mal in 1. version ausprogrammiert.

--- aufgabe1/src/OOP/Konfiguration.java
+++ aufgabe1/src/OOP/Konfiguration.java
@@ -1,5 +1,42 @@
 package OOP;
 
-public class Konfiguration {
+import java.util.HashMap;
 
+import OOP.ProduktVerwaltung.Produkt;
+
+/**
+ * 
+ * @author sebastian
+ *
+ */
+public class Konfiguration {
+	private HashMap<Produkt, Integer> products;
+	
+	Konfiguration() {
+		products = new HashMap<Produkt, Integer>();
+	}
+	
+	/**
+	 * Fuegt ein neues Produkt hinzu.
+	 */
+	public void addProduct(Produkt p, int quantity) {
+		if (products.containsKey(p)) {
+			int current = products.get(p);
+			current += quantity;
+			products.put(p, current);
+		}
+		else {
+			products.put(p, quantity);
+		}
+	}
+	
+	/**
+	 * Verringert den Lagerbestand im angegebenen Lager.
+	 */
+	public void removeFromStock(Lager l) {
+		for (Produkt p: products.keySet()) {
+			int quantity = products.get(p);
+			l.decrementStock(p, quantity);
+		}
+	}
 }
