0eecf561aa1063e07580f4d1267deb5134a89e66
Benjamin M <e0826765@student.tuwien.ac.at>
2010-11-05 18:07:24 +0100

    Added some tests to Test.java

--- aufgabe4/src/Test.java
+++ aufgabe4/src/Test.java
@@ -2,12 +2,40 @@
 public class Test {
 
 	public static void main(String[] args) {
+		/* NOTE: An mein Team
+		 * Bitte verwendet einfach die 2 code Strings hier. Der eine enthält (fast) Alle Dinge die man so umformatieren
+		 * soll und der zweite ist aus der Angabe und gut zum vergleichen. Wenn euch noch was einfällt, was fehlt,
+		 * kann man code1 anpassen oder auch einen zusätzlichen String einführen. Aber es sollte nicht zu viele werden
+		 * Nachtrag: Hab code1 in code1 und code1p2 (part 2) aufgeteilt, weil in der Angabe steht:
+		 * transform(x)+transform(y) = transform(x+y) und das wollte ich testen.
+		 */
+		
+		// TODO: More tests with OO-features
+		
+		// Test Strings
+		String code1 = "  public void main(int argc, char[] argv) {  int a=0, b=10, c=0;c=a+b; if (a == 0) {\t";
+		String code1p2 = "//Foo      \n  call();} foobar();}";
+		String code2 = "class A{ B c; ;D f { c.f(d) /*x*/; /*y*/} }";
+		
+		// Testing LongPretty
 		LongPretty lp = new LongPretty(4);
-		String code = "  public void main(int argc, char[] argv) {  int a=0, b=10, c=0;c=a+b; " +
-				"//Foo      \n  call();}";
-		System.out.println("Unformatted:\n"+code+"\nLongPretty Formatted:\n"+lp.transform(code)+"\n");
-		code = "class A{ B c; ;D f { c.f(d) /*x*/; /*y*/} }";
-		System.out.println("Unformatted:\n"+code+"\nLongPretty Formatted:\n"+lp.transform(code)+"\n");
+		System.out.println("*** Testing LongPretty ***");
+		System.out.println("Unformatted code1: "+code1+"\nLongPretty Formatted:\n"+lp.transform(code1)+"\n");
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nLongPretty Formatted:\n"+lp.transform(code1p2)+"\n");
+		System.out.println("Unformatted code1 zusammen:\n"+code1+code1p2+"\nLongPretty Formatted:\n"+lp.transform(code1+code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code2+"\nLongPretty Formatted:\n"+lp.transform(code2)+"\n");
+		System.out.println("Testing 2 code fragments with Reset() call inbetween:");
+		System.out.println("Unformatted code1: "+code1+"\nLongPretty Formatted:\n"+lp.transform(code1)+"\n");
+		lp.reset();
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nLongPretty Formatted:\n"+lp.transform(code1p2)+"\n");
+		
+		// Testing LongPretty
+		DepthPretty dp = new LongPretty(4);
+		System.out.println("*** Testing LongPretty as DepthPretty ***");
+		System.out.println("Unformatted code1: "+code1+"\nLongPretty Formatted:\n"+dp.transform(code1)+"\n");
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nLongPretty Formatted:\n"+dp.transform(code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code1+code1p2+"\nLongPretty Formatted:\n"+dp.transform(code1+code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code2+"\nLongPretty Formatted:\n"+dp.transform(code2)+"\n");
 	}
 
 }
