9888bfbc06e3130cf3692db581f96011cc20bac8
mo-oz <Monika@.(none)>
2010-11-09 02:26:00 +0100

    implemented ShortPretty (without stringbuilder) and tests added

--- aufgabe4/src/ShortPretty.java
+++ aufgabe4/src/ShortPretty.java
@@ -1,5 +1,4 @@
 
-
 public class ShortPretty extends DepthPretty {
 
 	public ShortPretty(int depth) {
@@ -8,7 +7,69 @@ public class ShortPretty extends DepthPretty {
 
 	@Override
 	public String transform(String s) {
-		return null;
+		char si;
+		String newS ="";
+		boolean newline = true;
+		
+		for(int i = 0; i < s.length(); i++) {
+			si = s.charAt(i);
+			
+			if(si == '{'){
+				newS += "{\n";
+				pos += depth;
+				newline = true;
+			}
+			else if(si == '}') {
+				if(pos>0) {
+					pos -= depth;
+					newS += this.getPosSpaces()+"}\n";
+				}
+				else {
+					newS += "}\n";
+				}
+				newline = true;
+			}
+			else if(si == ';') {
+				if(!newline) {
+					newS += ";\n";
+				} else {
+					newS += this.getPosSpaces()+";\n";
+				}
+				newline = true;
+			}
+			else if(si == '\n') {
+				newS += "\n";
+				newline = true;
+			}
+			else if(si == '*'){
+				if(s.charAt(i+1) == '/') {
+					if(s.charAt(i+2) != ';') {
+						newS += "*/"+'\n';
+						newline = true;
+						i++;
+					} else {
+						newS += "*";
+					}
+				}
+				else {
+					newS += "*";
+				}
+			}
+			else if(si == ' ' || si == '\t') {
+				if(!newline) {
+					newS += si;
+				}
+				this.getPosSpaces();
+			}
+			else {
+				if(newline) {
+					newS += this.getPosSpaces();
+				}
+				newS += si;
+				newline = false;
+			}
+		}
+		return newS;
 	}
 
 }
--- aufgabe4/src/Test.java
+++ aufgabe4/src/Test.java
@@ -47,13 +47,33 @@ public class Test {
 		System.out.println("Unformatted code1 part 2: "+code1p2+"\nLongPretty Formatted:\n"+lp.transform(code1p2)+"\n");
 		
 		// Testing LongPretty
-		DepthPretty dp = new LongPretty(4);
+		DepthPretty dpl = new LongPretty(4);
 		System.out.println("*** Testing LongPretty as DepthPretty ***");
-		System.out.println("Unformatted code1: "+code1+"\nLongPretty Formatted:\n"+dp.transform(code1)+"\n");
-		System.out.println("Unformatted code1 part 2: "+code1p2+"\nLongPretty Formatted:\n"+dp.transform(code1p2)+"\n");
-		System.out.println("Unformatted:\n"+code1+code1p2+"\nLongPretty Formatted:\n"+dp.transform(code1+code1p2)+"\n");
-		System.out.println("Unformatted:\n"+code2+"\nLongPretty Formatted:\n"+dp.transform(code2)+"\n");
+		System.out.println("Unformatted code1: "+code1+"\nLongPretty Formatted:\n"+dpl.transform(code1)+"\n");
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nLongPretty Formatted:\n"+dpl.transform(code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code1+code1p2+"\nLongPretty Formatted:\n"+dpl.transform(code1+code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code2+"\nLongPretty Formatted:\n"+dpl.transform(code2)+"\n");
 		
+		// Testing ShortPretty
+		ShortPretty sp = new ShortPretty(4);
+		System.out.println("*** Testing ShortPretty ***");
+		System.out.println("Unformatted code1: "+code1+"\nShortPretty Formatted:\n"+sp.transform(code1)+"\n");
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nShortPretty Formatted:\n"+sp.transform(code1p2)+"\n");
+		System.out.println("Unformatted code1 zusammen:\n"+code1+code1p2+"\nShortPretty Formatted:\n"+sp.transform(code1+code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code2+"\nShortPretty Formatted:\n"+sp.transform(code2)+"\n");
+		System.out.println("Testing 2 code fragments with Reset() call inbetween:");
+		System.out.println("Unformatted code1: "+code1+"\nShortPretty Formatted:\n"+sp.transform(code1)+"\n");
+		sp.reset();
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nShortPretty Formatted:\n"+sp.transform(code1p2)+"\n");
+		
+		// Testing ShortPretty
+		DepthPretty dps = new ShortPretty(4);
+		System.out.println("*** Testing ShortPretty as DepthPretty ***");
+		System.out.println("Unformatted code1: "+code1+"\nShortPretty Formatted:\n"+dps.transform(code1)+"\n");
+		System.out.println("Unformatted code1 part 2: "+code1p2+"\nShortPretty Formatted:\n"+dps.transform(code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code1+code1p2+"\nShortPretty Formatted:\n"+dps.transform(code1+code1p2)+"\n");
+		System.out.println("Unformatted:\n"+code2+"\nShortPretty Formatted:\n"+dps.transform(code2)+"\n");
+	
 		// Testing VarPretty
 		VarPretty vp = new VarPretty(VarPretty.Mode.Long, 4);
 		System.out.println("*** Testing VarPretty with LongPretty and depth 4 ***");
