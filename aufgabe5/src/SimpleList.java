31b5016a5663352c3c32d6fdedbbf6be7a3a6e01
Benjamin M <e0826765@student.tuwien.ac.at>
2010-11-23 00:27:22 +0100

    Coded SimpleList

--- aufgabe5/src/SimpleList.java
+++ aufgabe5/src/SimpleList.java
@@ -1,3 +1,48 @@
-public class SimpleList {
-
+public class SimpleList<T> {
+	int pos;
+	T element;
+	SimpleList<T> next;
+	
+	public SimpleList() {
+		pos = 0;
+		next = null;
+		element = null;
+	}
+	
+	private SimpleList(int i) {
+		pos = i;
+		next = null;
+		element = null;
+	}
+	
+	public T get(int i) {
+		if (pos == i) {
+			return element;
+		} else if (i > pos) {
+			 if (next != null) {
+				 return next.get(i);
+			 } else {
+				 return null;
+			 }
+		} else {
+			return null;
+		}
+	}
+	
+	public void add(T element) {
+		if (next == null) {
+			this.element = element;
+			next = new SimpleList<T>(pos+1);
+		} else {
+			next.add(element);
+		}
+	}
+	
+	public int size() {
+		if (next == null) {
+			return pos;
+		} else {
+			return next.size();
+		}
+	}
 }
--- aufgabe5/src/Test.java
+++ aufgabe5/src/Test.java
@@ -5,8 +5,6 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
