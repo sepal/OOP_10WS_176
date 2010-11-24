7c8db8f51da69f3c3ed751860e468f4d005cdb27
Sepal <sep.gil@gmail.com>
2010-11-24 03:33:03 +0100

    Added some tests

--- aufgabe5/src/Test.java
+++ aufgabe5/src/Test.java
@@ -5,6 +5,24 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("*** Testing Interator<MyInt>***");
+		Tree<MyInt> intTree = new Tree<MyInt>();
+		intTree.assoc().insert(new MyInt(1));
+		intTree.assoc().insert(new MyInt(3));
+		intTree.assoc().insert(new MyInt(2));
+		AssocIter<MyInt, AssocIter<MyInt, ?>> it = intTree.assoc();
+		it.next();
+		// TODO: checken wieso des net funzt.
+		AssocIter<MyInt, AssocIter<MyInt, ?>> subit = it.assoc();
+		subit.insert(new MyInt(10));
+		intTree.assoc().delete();
+		intTree.allLabels();
+		
+
+		System.out.println("*** Testing Interator<MyInt>***");
+		ValueTree<Descriptor, BDescriptor> descTree = new ValueTree<Descriptor, BDescriptor>();
+		descTree.assoc().insert(new ADescriptor("lalalAla"), new BDescriptor("blaBlablabla"));
+		
 
 	}
 }
