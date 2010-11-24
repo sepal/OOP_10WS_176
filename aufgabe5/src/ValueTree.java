d68095fca897593554f09bb887aca48e8383cc88
Sepal <sep.gil@gmail.com>
2010-11-24 02:57:39 +0100

    Added function headers to ValueTree."

--- aufgabe5/src/ValueTree.java
+++ aufgabe5/src/ValueTree.java
@@ -1,4 +1,47 @@
 
-public class ValueTree {
 
+public class ValueTree<L extends Comparable<L>, V> extends Tree {
+	V value;
+	
+
+	public AssocIter<L, AssocIter<L, ?>> assoc() {
+		return new ValueIter(this);
+	}
+	
+	protected class ValueIter implements AssocIter<L, AssocIter<L, ?>> {
+		
+		private ValueIter(Tree<L> start) {
+
+		}
+		
+		@Override
+		public L next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public AssocIter<L, ?> assoc() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean insert(L element) {
+			return false;
+		}
+
+		@Override
+		public boolean delete() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		
+	}
 }
