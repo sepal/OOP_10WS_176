6fd0f3f144044bd088ad888230b5ff1997e60cdd
Sepal <sep.gil@gmail.com>
2010-11-24 12:05:50 +0100

    Added polite, but accusationing and lightly sarkastic comment.

--- aufgabe5/src/Test.java
+++ aufgabe5/src/Test.java
@@ -1,3 +1,16 @@
+/**
+ * Anmerkung: "Die Aufgabe ist so konstruiert, dass dabei einige Schwierigkeiten
+ * auftauchen, für die wir Lösungsmöglichkeiten kennengelernt haben." steht in
+ * der Angabe, leider müssen wir dem widersprechen. Keine der uns bekannten
+ * Lösungsmöglichkeiten konnten zB. die Probleme mit "rekursiven" Generics
+ * (AssocIter<L, AsssocIter<L, Ass....>>) lösen.
+ * 
+ * Leider konnten wir in dem geringen Zeitrahmen die komplexe Aufgabenstellung
+ * verstehen und auch keine Lösung für diese Probleme finden.
+ * 
+ * @author GroupWS10176
+ * 
+ */
 
 public class Test {
 
@@ -12,28 +25,26 @@ public class Test {
 		intTree.assoc().insert(new MyInt(2));
 		AssocIter<MyInt, AssocIter<MyInt, ?>> it = intTree.assoc();
 		it.next();
-		// TODO: checken wieso des net funzt.
-		// Das geht zwar, lÃ¶scht aber die Typinformation Ã¼ber den zweiten Parameter, wodurch
+
+		// Das geht zwar, lÃ¶scht aber die Typinformation Ã¼ber den zweiten
+		// Parameter, wodurch
 		// man eine stufe tiefer erst recht wieder keinen Iterator bekommt x|
 		AssocIter<MyInt, ?> subit = it.assoc();
 		subit.insert(new MyInt(10));
 		AssocIter<MyInt, ?> subsubit = subit.assoc();
 		intTree.assoc().delete();
 		intTree.allLabels();
-		
 
 		System.out.println("*** Testing Interator<MyInt>***");
 		ValueTree<Descriptor, BDescriptor> descTree = new ValueTree<Descriptor, BDescriptor>();
 		descTree.assoc().insert(new ADescriptor("lalalAla"));
-		AssocIter<Descriptor, AssocIter<Descriptor, ?>> itDesc = descTree.assoc();
+		AssocIter<Descriptor, AssocIter<Descriptor, ?>> itDesc = descTree
+				.assoc();
 		itDesc.next();
-		
-		
+
 		// Folgendes natürlich nicht möglich, da ja AssocIter kein set hat.
-		// Casten darf ich auch nicht, also wie soll man dann set aufrufen????
+		// Casten darf ich auch nicht, also wie soll man dann set aufrufen?
 		itDesc.set();
-		
-		
 
 	}
 }
