d7a645a80edae51ed3ac1054311bf37a8dcb5563
Sepal <sep.gil@gmail.com>
2010-11-24 02:05:36 +0100

    Manual merge from monika_branch.

--- aufgabe5/src/ADescriptor.java
+++ aufgabe5/src/ADescriptor.java
@@ -5,4 +5,15 @@ public class ADescriptor extends Descriptor {
 		super(desc);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public int as(String s) {
+		int count = 0;
+		for(int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if(ch == 'a' || ch =='A') {
+				count++;
+			}
+		}
+		return count;
+	}
 }
--- aufgabe5/src/BDescriptor.java
+++ aufgabe5/src/BDescriptor.java
@@ -5,4 +5,14 @@ public class BDescriptor extends Descriptor {
 		super(desc);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public boolean bs(String s) {
+		for(int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if(ch == 'b' || ch == 'B') {
+				return true;
+			}
+		}
+		return false;
+	}
 }
