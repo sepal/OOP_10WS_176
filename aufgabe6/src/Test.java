99503df33877adb73324eb9c439dafa0d2212af4
mo-oz <Monika@.(none)>
2010-11-26 15:52:06 +0100

    start test.java

--- aufgabe6/src/Test.java
+++ aufgabe6/src/Test.java
@@ -5,8 +5,17 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("***New CarPool***");
+		CarPool cp1 = new CarPool("Fuhrpark1");
+		System.out.println("***New Cars***");
+		Car ec1 = new ElectricCar(01);
+		Car ec2 = new ElectricCar(02);
+		Car ec3 = new ElectricCar(03);
+		Car fc1 = new FuelCar(04);
+		Car fc2 = new FuelCar(05);
+		Car fc3 = new FuelCar(06);
+		
+		
 	}
 
 }
