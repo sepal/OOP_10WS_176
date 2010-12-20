29b1e5bd308425af157c9f83a1f01b1c781d79c0
Sebastian <sep.gil@gmail.com>
2010-12-20 14:25:25 +0100

    Added constructor for field.

--- aufgabe8/src/Field.java
+++ aufgabe8/src/Field.java
@@ -1,4 +1,13 @@
 
 public class Field {
-
+	// The position coordinates
+	private int x,y;
+	private boolean[][] wall;
+	
+	public Field(int x, int y, boolean[][] wall) {
+		this.x = x;
+		this.y = y;
+		this.wall = wall;
+	}
+	
 }
