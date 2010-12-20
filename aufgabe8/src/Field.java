8f5b0768ac7ec2423762635f6e764ffed23e3810
Sebastian <sep.gil@gmail.com>
2010-12-20 14:40:38 +0100

    Modified field constructor.

--- aufgabe8/src/Field.java
+++ aufgabe8/src/Field.java
@@ -1,13 +1,26 @@
 
 public class Field {
+	public static final int NORTH = 0;
+	public static final int EAST = 1;
+	public static final int SOUTH = 2;
+	public static final int WEST = 3;
+
 	// The position coordinates
 	private int x,y;
-	private boolean[][] wall;
+	private boolean[] wall;
 	
-	public Field(int x, int y, boolean[][] wall) {
+	/**
+	 * precondition: 
+	 * Every parameter should be > 0, wall should.length == 4, 
+	 * Field constants should be used to set the right walls.
+	 * 
+	 * postcondition:
+	 * A new field object was created.
+	 */
+	public Field(int x, int y, boolean[] wall) {
 		this.x = x;
 		this.y = y;
 		this.wall = wall;
+		wall[NORTH] = true;
 	}
-	
 }
