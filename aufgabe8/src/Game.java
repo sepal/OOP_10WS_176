ab9d81a19217ee50b8409736c13e855c7eb3ea07
Sebastian <sep.gil@gmail.com>
2010-12-20 15:10:44 +0100

    Implemented some game functions.

--- aufgabe8/src/Game.java
+++ aufgabe8/src/Game.java
@@ -12,11 +12,25 @@ public class Game {
 	
 	private Labyrinth lab;
 	private State state;
+	private int sleepTime;
 	
-	private List <Integer> characters;
+	private List <Character> characters;
 	
-	public Game(int width, int height) {
+	public Game(int width, int height, int sleepTime) {
 		lab = new Labyrinth(width, height);
-		characters = Collections.synchronizedList(new ArrayList<Integer>());
+		characters = Collections.synchronizedList(new ArrayList<Character>());
+		this.sleepTime = sleepTime;
+	}
+	
+	private void addCharacter(Character character) {
+		characters.add(character);
+	}
+	
+	private void createGhost(int x, int y) {
+		addCharacter(new Ghost(x, y, sleepTime));
+	}
+	
+	private void createHunter(int x, int y) {
+		addCharacter(new Hunter(x, y, sleepTime));
 	}
 }
