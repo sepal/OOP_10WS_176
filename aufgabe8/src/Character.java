4838447813f0d7095839a970e6f1769b9f2f5500
mo-oz <Monika@.(none)>
2010-12-21 16:22:53 +0100

    zusicherungen character

--- aufgabe8/src/Character.java
+++ aufgabe8/src/Character.java
@@ -6,20 +6,29 @@ public abstract class Character implements Runnable {
 
 	private int positionx;
 	private int positiony;
-	private int treasure;
 	private int time;
 	private Game game;
 	private boolean running;
-	ArrayList<Field> way;
-
+	private ArrayList<Field> way;
+	
+	/**
+	 *(precondition) game must exist, other parameters must be >= 0, x < lab.width, y < lab.height
+	 *(postcondition) creates new character at a startposition in a game, way contains only startpoint, 
+	 *because hasn't walked yet 
+	 */
 	public Character(int time, int x, int y, Game game) {
 		this.time = time;
 		this.positionx = x;
 		this.positiony = y;		
 		this.game = game;
 		way = new ArrayList<Field>();
+		Field f = this.game.getLabyrith().getField(x, y); //start point
+		way.add(f);
 	}
 	
+	/**
+	 *(postcondition) character walks while game is running, thread sleeps after each position change 
+	 */
 	@Override
 	public void run() {
 		this.running = true;
@@ -33,6 +42,9 @@ public abstract class Character implements Runnable {
 		}
 	}
 
+	/**
+	 *(postcondition) character walks
+	 */
 	protected void walk(){
 		
 	}
