26d501e01ed7582ea89f2d52e3265549c448035b
mo-oz <Monika@.(none)>
2010-12-21 15:54:24 +0100

    zusicherungen in lab

--- aufgabe8/src/Labyrinth.java
+++ aufgabe8/src/Labyrinth.java
@@ -9,6 +9,9 @@ public class Labyrinth {
 	private int h = 0;
 	private int w = 0;
 	
+	/**
+	 *(postcondition) creates new labyrinth with treasures on fields
+	 */
 	public Labyrinth(boolean[][][] labConfig) {
 		int width = labConfig.length;
 		int height = labConfig[0].length;
@@ -24,18 +27,34 @@ public class Labyrinth {
 		
 	}
 	
+	/**
+	 *(precondition) x and y must be > 0 && x < width && y < height
+	 *(postcondition) returns field
+	 */
 	public Field getField(int x, int y) {
 		return lab[y][x];
 	}
 	
+	/**
+	 *(precondition) field must exist
+	 *(postcondition) returns true, if there is no northern wall, otherwise false
+	 */
 	public boolean goNorth(Field f) {
 		return f.hasWall(Field.NORTH);
 	}
 	
+	/**
+	 *(precondition) field must exist
+	 *(postcondition) returns true, if there is no eastern wall, otherwise false
+	 */
 	public boolean goEast(Field f) {
 		return f.hasWall(Field.EAST);
 	}
 	
+	/**
+	 *(precondition) field must exist
+	 *(postcondition) returns true, if there is no southern wall(== lower neighbor has no northern wall), otherwise false
+	 */
 	public boolean goSouth(Field f) {
 		if(f.hasWall(Field.SOUTH) || lab[f.getX()][f.getY()+1].hasWall(Field.NORTH)) {
 			return true;
@@ -44,6 +63,10 @@ public class Labyrinth {
 		}
 	}
 	
+	/**
+	 *(precondition) field must exist
+	 *(postcondition) returns true, if there is no western wall(==left neighbor has no eastern wall), otherwise false
+	 */
 	public boolean goWest(Field f) {
 		if(f.hasWall(Field.WEST) || lab[f.getX()-1][f.getY()].hasWall(Field.EAST)) {
 			return true;
@@ -52,6 +75,10 @@ public class Labyrinth {
 		}
 	}
 	
+	/**
+	 *(precondition) field must exist
+	 *(postcondition) returns true, if field is exit field, otherwise false
+	 */
 	public boolean onWinField(Field f) {
 		if (f.getX() == this.width-1 || f.getY() == 0) {
 			return true;
@@ -59,10 +86,18 @@ public class Labyrinth {
 		return false;
 	}
 
+	/**
+	 *(precondition) labyrinth must exist
+	 *(postcondition) returns width
+	 */
 	public int getWidth() {
 		return width;
 	}
 
+	/**
+	 *(precondition) labyrinth must exist
+	 *(postcondition) returns height
+	 */
 	public int getHeight() {
 		return height;
 	}
