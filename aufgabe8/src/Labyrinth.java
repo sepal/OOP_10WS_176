ad55045eace5c39aaf3446d5821e96365915fcdd
mo-oz <Monika@.(none)>
2010-12-21 11:27:40 +0100

    lab output toString()

--- aufgabe8/src/Labyrinth.java
+++ aufgabe8/src/Labyrinth.java
@@ -2,8 +2,10 @@ import java.util.Random;
 
 
 public class Labyrinth {
-
+	
 	private Field[][] lab;
+	private int width;
+	private int height;
 	
 	public Labyrinth(int width, int height) {
 		this.lab = new Field[height][width];
@@ -41,10 +43,56 @@ public class Labyrinth {
 				
 				Field f = new Field(j, i, walls, treasure);
 				lab[j][i] = f;
+				//createLab();
 				
 			}
 		}
 		
+	}
+	
+	public char[][] createLab() {
+		height =height*2+1;
+		width = width*2+1;
+		char[][] c = new char[height][width];
+		for(int i = 1; i < height; i+=2) {
+			for(int j = 1; j < width; j+=2) {
+				Field f = getField(j, i);
+				boolean[] walls = f.getWall();
+				if(walls[0] == true) {
+					c[j][i-1] = '-';
+				} else {
+					c[j][i-1] = ' ';
+				}
+				if(walls[1] == true) {
+					c[j+1][i] = '|';
+				} else {
+					c[j+1][i] = ' ';
+				}
+				if(walls[2] == true) {
+					c[j][i+1] = '_';
+				} else {
+					c[j][i+1] = ' ';
+				}
+				if(walls[3] == true) {
+					c[j-1][i] = '|';
+				}
+				c[j][i] = ' ';
+			}
+		}
+		return c;
+	}
+	
+	public String toString() {
+		char[][] c = createLab();
+		String s = "";
+		
+		for(int i = 0; i < height; i++) {
+			for(int j = 0;j < width; j++) {
+				s += c[j][i];
+			}
+		}
+		
+		return s;
 	}
 	
 	public Field getField(int x, int y) {
@@ -73,5 +121,13 @@ public class Labyrinth {
 		} else {
 			return false;
 		}
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 }
